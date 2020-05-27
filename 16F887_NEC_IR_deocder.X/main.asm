89cf3ff28c607fbfe835674808e03e62b0768e92
dsoze1138 <dsoze1138@gmail.com>
2020-05-27 12:38:01 -0700

    Refactor setting configuration words

--- 16F887_NEC_IR_deocder.X/main.asm
+++ 16F887_NEC_IR_deocder.X/main.asm
@@ -52,16 +52,6 @@
 ;                 +-----------------------:
 ;                          DIP-40
 ;
-; Include Special Function Register definitions
-;
-#include "p16f887.inc"
-;
-; PIC16F887 Configuration Bit Settings
-; Assembly source line config statements
-;
- __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_ON & _FCMEN_OFF & _LVP_OFF
- __CONFIG _CONFIG2, _BOR4V_BOR21V & _WRT_OFF
-;
 ; Power on reset vector
 ;
 RES_VECT    CODE    0x0000      ; processor reset vector
